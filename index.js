9e0dc817aaa5cff0f0a4b1d4a51fedecf4d874d4
“Snappyhacker” <jimodhiambo515@gmail.com>
2024-07-05 22:39:08 +0300

    dom element

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Jim Henry is the champion'; 
+
+document.body.appendChild(newHeader);
